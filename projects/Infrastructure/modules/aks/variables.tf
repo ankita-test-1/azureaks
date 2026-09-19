17d927d5c9945adfdb38ef3ff5869355fa4c8d0e
ankita-test-1 <ankita.rath.96@gmail.com>
2026-09-19 20:23:46 +0530

    Update and rename variables.tf to variables.tf

---
+++ projects/Infrastructure/modules/aks/variables.tf
@@ -0,0 +1,66 @@
+variable "cluster_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "vnet_id" {
+  description = "Needed to link the private DNS zone into the VNet"
+  type        = string
+}
+
+variable "node_subnet_id" {
+  type = string
+}
+
+variable "acr_id" {
+  description = "Resource ID of the ACR, for the AcrPull role assignment"
+  type        = string
+}
+
+variable "system_vm_size" {
+  type    = string
+  default = "Standard_B2s"
+}
+
+variable "vm_size" {
+  description = "AKS node pools take a single VM size, unlike EKS's list of candidate instance types"
+  type        = string
+}
+
+variable "node_group_name" {
+  type = string
+}
+
+variable "capacity_type" {
+  type    = string
+  default = "ON_DEMAND" # or "SPOT"
+}
+
+variable "desired_size" {
+  type = number
+}
+
+variable "min_size" {
+  type = number
+}
+
+variable "max_size" {
+  type = number
+}
+
+variable "disk_size" {
+  type    = number
+  default = 30
+}
+
+variable "zones" {
+  type    = list(string)
+  default = ["1", "2", "3"]
+}
