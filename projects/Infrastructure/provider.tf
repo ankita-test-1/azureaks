ba80a7675f70ddc9357d4d9d5e54a9f56a36c804
ankita-test-1 <ankita.rath.96@gmail.com>
2026-09-19 21:15:16 +0530

    Update provider.tf

--- projects/Infrastructure/provider.tf
+++ projects/Infrastructure/provider.tf
@@ -20,7 +20,7 @@ terraform {
     resource_group_name  = "tfstate-rg"
     storage_account_name = "akslearntfstate"
     container_name        = "tfstate"
-    key                    = "infrastructure.tfstate"
+    key                    = "infrastructure-tfstate"
     use_oidc               = true
   }
 }
