cdce87e4e863e5cdcac0af7c467acce7853ee016
ankita-test-1 <ankita.rath.96@gmail.com>
2026-09-19 21:38:32 +0530

    Update main.tf

--- projects/Infrastructure/main.tf
+++ projects/Infrastructure/main.tf
@@ -48,6 +48,14 @@ module "aks" {
   depends_on = [module.vnet, module.acr]
 }
 
+provider "kubernetes" {
+  alias                  = "aks"
+  host                   = module.aks.kube_config.host
+  client_certificate     = base64decode(module.aks.kube_config.client_certificate)
+  client_key             = base64decode(module.aks.kube_config.client_key)
+  cluster_ca_certificate = base64decode(module.aks.kube_config.cluster_ca_certificate)
+}
+
 provider "helm" {
   alias = "aks"
 
