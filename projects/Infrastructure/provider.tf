523152ed5bc468b42f955e5ebc9642de94fc22f4
ankita-test-1 <ankita.rath.96@gmail.com>
2026-09-19 21:19:16 +0530

    Update provider.tf

--- projects/Infrastructure/provider.tf
+++ projects/Infrastructure/provider.tf
@@ -19,7 +19,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
     storage_account_name = "akslearntfstate"
-    container_name        = "tfstate"
+    container_name        = "infrastructure-tfstate"
     key                    = "infrastructure-tfstate"
     use_oidc               = true
   }
