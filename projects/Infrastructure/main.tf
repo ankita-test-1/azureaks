307f7f9ec03013ba88d78a35564597e7fa2af185
ankita-test-1 <ankita.rath.96@gmail.com>
2026-09-19 20:29:23 +0530

    Update main.tf

--- projects/Infrastructure/main.tf
+++ projects/Infrastructure/main.tf
@@ -1,66 +1,81 @@
-module "vpc" {
-  source = "./modules/vpc"
-
-  vpc_name     = var.vpc_name
-  cidr_block   = var.vpc_cidr
-  subnet_cidrs = [for s in var.subnets : s.cidr_block]
-  availability_zones = [for s in var.subnets : s.availability_zone]
-  cluster_name     = var.cluster_name
+resource "azurerm_resource_group" "this" {
+  name     = var.resource_group_name
+  location = var.location
 }
 
+module "vnet" {
+  source = "./modules/vpc" # rename to ./modules/vnet whenever you do the folder renames
 
-module "eks" {
-  source = "./modules/eks"
-
-  cluster_name     = var.cluster_name
-  node_group_name  = var.node_group_name
-
-  instance_types = var.instance_types
-  min_size       = var.min_size
-  desired_size   = var.desired_size
-  max_size       = var.max_size
-
-  subnet_ids = module.vpc.subnet_ids
-  depends_on = [module.vpc]
+  vnet_name             = var.vnet_name
+  location              = azurerm_resource_group.this.location
+  resource_group_name   = azurerm_resource_group.this.name
+  address_space         = var.address_space
+  node_subnet_cidr      = var.node_subnet_cidr
+  zones                 = var.zones
+  enable_bastion_subnet = var.enable_bastion_subnet
+  bastion_subnet_cidr   = var.bastion_subnet_cidr
 }
 
-module "ecr" {
-  source = "./modules/ecr"
+module "acr" {
+  source = "./modules/ecr" # rename to ./modules/acr whenever you do the folder renames
 
-  repositories = var.repositories
+  acr_name             = var.acr_name
+  location             = azurerm_resource_group.this.location
+  resource_group_name  = azurerm_resource_group.this.name
+  sku                  = var.acr_sku
+  repositories         = var.repositories
 }
 
+module "aks" {
+  source = "./modules/eks" # rename to ./modules/aks whenever you do the folder renames
 
-data "aws_eks_cluster_auth" "eks" {
-  name = module.eks.cluster_name
+  cluster_name        = var.cluster_name
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  vnet_id             = module.vnet.vnet_id
+  node_subnet_id      = module.vnet.node_subnet_id
+  acr_id              = module.acr.acr_id
+
+  system_vm_size  = var.system_vm_size
+  vm_size         = var.vm_size
+  node_group_name = var.node_group_name
+  capacity_type   = var.capacity_type
+
+  desired_size = var.desired_size
+  min_size     = var.min_size
+  max_size     = var.max_size
+  disk_size    = var.disk_size
+  zones        = var.zones
+
+  depends_on = [module.vnet, module.acr]
 }
 
 provider "kubernetes" {
-  alias                  = "eks"
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.eks.token
+  alias                  = "aks"
+  host                   = module.aks.kube_config.host
+  client_certificate     = base64decode(module.aks.kube_config.client_certificate)
+  client_key             = base64decode(module.aks.kube_config.client_key)
+  cluster_ca_certificate = base64decode(module.aks.kube_config.cluster_ca_certificate)
 }
 
 provider "helm" {
-  alias = "eks"
+  alias = "aks"
 
-  kubernetes =  {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.eks.token
+  kubernetes = {
+    host                   = module.aks.kube_config.host
+    client_certificate     = base64decode(module.aks.kube_config.client_certificate)
+    client_key             = base64decode(module.aks.kube_config.client_key)
+    cluster_ca_certificate = base64decode(module.aks.kube_config.cluster_ca_certificate)
   }
 }
-
 
 module "argocd" {
   source = "./modules/argocd"
 
   providers = {
-    kubernetes = kubernetes.eks
-    helm       = helm.eks
+    kubernetes = kubernetes.aks
+    helm       = helm.aks
   }
 
-  depends_on = [module.eks]
+  depends_on = [module.aks]
 }
-
