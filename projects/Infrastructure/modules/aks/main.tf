c7cfa02d76c8a36da54ca1df25bf254a4b580609
ankita-test-1 <ankita.rath.96@gmail.com>
2026-09-25 21:04:59 +0530

    Update main.tf

--- projects/Infrastructure/modules/aks/main.tf
+++ projects/Infrastructure/modules/aks/main.tf
@@ -70,7 +70,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   lifecycle {
     ignore_changes = [default_node_pool[0].node_count],
-    network_profile[0].pod_cidr
+    network_profile[0].pod_cidr,
   }
 }
 
